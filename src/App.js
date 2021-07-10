0c5039d995858b5c021fc111752218814ca6ed45
aliasghar <aliamsalehi@gmail.com>
2021-07-11 00:27:19 +0430

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {useEffect, useState, useLayoutEffect} from 'react'
+import html2pdf from "html2pdf.js";
+
+
+function App() {
+    return <div>
+        hellowwwwwww
+    </div>
+}
+
+export default App;
+
